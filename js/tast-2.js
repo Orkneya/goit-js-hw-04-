b4a4226d01030d1b3da1e0ed8cc0e66270ffda90
Orkneya <orkneya17@gmail.com>
2024-12-18 00:02:00 +0200

    first

---
+++ js/tast-2.js
@@ -0,0 +1,14 @@
+function calcAverageCalories(days){
+
+}
+console.log(
+    calcAverageCalories([
+      { day: "monday", calories: 3010 },
+      { day: "tuesday", calories: 3200 },
+      { day: "wednesday", calories: 3120 },
+      { day: "thursday", calories: 2900 },
+      { day: "friday", calories: 3450 },
+      { day: "saturday", calories: 3280 },
+      { day: "sunday", calories: 3300 }
+    ])
+  );
